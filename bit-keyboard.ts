4735e8da3fbb4218e467068bfba4e7b1fe83eebf
derrick <536567043@qq.com>
2018-05-24 16:12:38 +0800

    添加bit-keyboard模块

---
+++ bit-keyboard.ts
@@ -0,0 +1,64 @@
+/**
+ * IKBIT PACKAGES
+ */
+//% weight=1000 color=#436EEE icon="\uf1cb"
+namespace ikbit { 
+    const BIT_KEYBOARD_ADD = 0X05;
+    const REG_KB_SHORT_RELEASE_BASE = 0X30;
+    const REG_KB_LONG_RELEASE_BASE = 0X40;
+    const REG_KB_LONG_HOLD_BASE = 0X50;
+    
+
+    export enum keyBoardEnum {
+        //% block="KEY0"
+        KEY_0 = 0,
+        //% block="KEY1"
+        KEY_1,
+        //% block="KEY2"
+        KEY_2,
+        //% block="KEY3"
+        KEY_3,
+        //% block="KEY4"
+        KEY_4,
+        //% block="KEY5"
+        KEY_5,
+        //% block="KEY6"
+        KEY_6,
+        //% block="KEY7"
+        KEY_7,
+        //% block="KEY8"
+        KEY_8,
+        //% block="KEY9"
+        KEY_9,
+        //% block="KEYA"
+        KEY_A,
+        //% block="KEYB"
+        KEY_B,
+        //% block="KEYC"
+        KEY_C,
+        //% block="KEYD"
+        KEY_D,
+        //% block="KEYE"
+        KEY_E,
+        //% block="KEYF"
+        KEY_F
+    }
+    //%block
+    export function keyBoardClickState(k: keyBoardEnum) :boolean{ 
+        let value = readReg8(BIT_KEYBOARD_ADD, REG_KB_SHORT_RELEASE_BASE + k);
+        if (value) return true;
+        else return true;
+    }
+    //%block
+    export function keyBoardLongClickState(k: keyBoardEnum) :boolean{ 
+        let value = readReg8(BIT_KEYBOARD_ADD, REG_KB_LONG_RELEASE_BASE + k);
+        if (value) return true;
+        else return true;
+    }
+    //%block
+    export function keyBoardLongHoldState(k: keyBoardEnum) :boolean{ 
+        let value = readReg8(BIT_KEYBOARD_ADD, REG_KB_LONG_HOLD_BASE + k);
+        if (value) return true;
+        else return true;
+    }
+}
